b434e57fd7ab8ff17469ce3a315fb5db8bfccd62
omurilolima <msl@cesar.school>
2025-08-20 13:00:37 +0100

    Add Appointment form + Click event listener

---
+++ appointment-app/src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
+
+@Component({
+  selector: 'app-appointment-list',
+  templateUrl: './appointment-list.component.html',
+  styleUrls: ['./appointment-list.component.css']
+})
+export class AppointmentListComponent {
+  
+  newAppointmentTitle : string = '';
+  newAppointmentDate : Date = new Date();
+
+  appointments: Appointment[] = []
+
+  addAppointment() {
+    alert(this.newAppointmentTitle + " " + this.newAppointmentDate);
+  }
+
+}
