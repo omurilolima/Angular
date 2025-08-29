ce28ff80bd5b326fcdc53975e0db1a74e2765792
omurilolima <msl@cesar.school>
2025-08-29 13:17:40 +0100

    Using ngOnInit to load appointments from local storage

--- appointment-app/src/app/appointment-list/appointment-list.component.ts
+++ appointment-app/src/app/appointment-list/appointment-list.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit {
   
   newAppointmentTitle : string = '';
   newAppointmentDate : Date = new Date();
 
   appointments: Appointment[] = []  // Initialize an empty array to hold appointments
+
+  ngOnInit(): void {
+    let savedAppointments = localStorage.getItem('appointments')
+
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : []
+  }
 
   addAppointment() {
     if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) {  // Check if title and date are valid
